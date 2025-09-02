c1a1713f1e9c7186bcd6ecbb11cca9c28ffcbe16
Davissantan4 <davi.ssilva@sptech.school>
2025-09-02 19:55:52 -0300

    Adicionando a versão atualizada do BD

--- BD/BD-script_criacao.sql
+++ BD/BD-script_criacao.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE IF NOT EXISTS autobotics;
+CREATE DATABASE autobotics;
 USE autobotics;
 
 -- Endereco
 CREATE TABLE endereco (
-  id_endereco INT AUTO_INCREMENT PRIMARY KEY,
+  id_endereco INT PRIMARY KEY AUTO_INCREMENT,
   estado VARCHAR(45),
   cidade VARCHAR(80),
   bairro VARCHAR(80),
@@ -13,46 +13,59 @@ CREATE TABLE endereco (
 
 -- Empresa / fabricante
 CREATE TABLE empresa (
-  id_empresa INT AUTO_INCREMENT PRIMARY KEY,
+  id_empresa INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(150) NOT NULL,
-  cnpj VARCHAR(20),
-  telefone VARCHAR(30),
+  cnpj VARCHAR(20) NOT NULL,
   fk_endereco INT,
-  status ENUM('PENDENTE', 'APROVADA', 'REPROVADA') NOT NULL DEFAULT 'PENDENTE'
-  CONSTRAINT fk_empresa_endereco FOREIGN KEY (fk_endereco) REFERENCES endereco(id_endereco)
+  status VARCHAR(20) NOT NULL DEFAULT 'PENDENTE', 
+  CONSTRAINT chk_status CHECK (status in ('PENDENTE', 'APROVADA', 'REPROVADA')),
+  CONSTRAINT fk_empresa_endereco FOREIGN KEY (fk_endereco) REFERENCES endereco(id_endereco),
+  UNIQUE ix_cnpj (cnpj)
 );
 
 -- Setor / departamento (vinculado a uma empresa)
 CREATE TABLE setor (
-  id_setor INT AUTO_INCREMENT PRIMARY KEY,
+  id_setor INT NOT NULL,
   fk_empresa INT NOT NULL,
   nome VARCHAR(120) NOT NULL,
   descricao TEXT,
-  regras JSON DEFAULT ('{}'),
-  CONSTRAINT fk_setor_empresa FOREIGN KEY (fk_empresa) REFERENCES empresa(id_empresa) ON DELETE CASCADE
+  CONSTRAINT fk_setor_empresa FOREIGN KEY (fk_empresa) REFERENCES empresa(id_empresa),
+  PRIMARY KEY (id_setor, fk_empresa)
+);
+
+-- Cargo
+CREATE TABLE cargo (
+  id_cargo INT PRIMARY KEY AUTO_INCREMENT,
+  cargo VARCHAR(50),
+  pode_editar TINYINT(1),
+  pode_excluir TINYINT(1),
+  pode_inserir TINYINT(1)
 );
 
 -- Funcionario (Separados em setores)
 CREATE TABLE funcionario (
-  id_funcionario INT AUTO_INCREMENT PRIMARY KEY,
-  fk_empresa INT,
-  fk_setor INT,
+  id_funcionario INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(150) NOT NULL,
-  email VARCHAR(150) UNIQUE,
-  senha_hash VARCHAR(255),
-  cpf VARCHAR(20),
-  cargo VARCHAR(100),
-  nivel_de_acesso VARCHAR(50),
+  email VARCHAR(150) NOT NULL,
+  senha_hash VARCHAR(255) NOT NULL,
+  cpf VARCHAR(20) NOT NULL,
   ativo TINYINT(1) DEFAULT 1,
+  fk_empresa INT NOT NULL,
+  fk_setor INT,
+  fk_cargo INT NOT NULL,
+  fk_superior INT,
   CONSTRAINT fk_funcionario_empresa FOREIGN KEY (fk_empresa) REFERENCES empresa(id_empresa),
-  CONSTRAINT fk_funcionario_setor FOREIGN KEY (fk_setor) REFERENCES setor(id_setor)
+  CONSTRAINT fk_funcionario_setor FOREIGN KEY (fk_setor) REFERENCES setor(id_setor),
+  CONSTRAINT fk_funcionario_cargo FOREIGN KEY (fk_cargo) REFERENCES cargo(id_cargo),
+  CONSTRAINT fk_funcionario_superior FOREIGN KEY (fk_superior) REFERENCES funcionario(id_funcionario),
+  UNIQUE ix_cpf (cpf),
+  UNIQUE ix_email (email)
 );
+
 
 -- Controlador (pertence a um setor;)
 CREATE TABLE controlador (
   id_controlador INT AUTO_INCREMENT PRIMARY KEY,
-  fk_empresa INT NOT NULL,
-  fk_setor INT NOT NULL,
   modelo VARCHAR(120) NOT NULL,
   numero_serial VARCHAR(120) UNIQUE,
   firmware VARCHAR(80),
@@ -60,19 +73,21 @@ CREATE TABLE controlador (
   modelo_cpu VARCHAR(100),
   ram_mb INT,
   storage_mb INT,
-  CONSTRAINT fk_controlador_empresa FOREIGN KEY (fk_empresa) REFERENCES empresa(id_empresa) ON DELETE CASCADE,
-  CONSTRAINT fk_controlador_setor FOREIGN KEY (fk_setor) REFERENCES setor(id_setor) ON DELETE CASCADE
+  fk_empresa INT NOT NULL,
+  fk_setor INT NOT NULL,
+  CONSTRAINT fk_controlador_empresa FOREIGN KEY (fk_empresa) REFERENCES empresa(id_empresa),
+  CONSTRAINT fk_controlador_setor FOREIGN KEY (fk_setor) REFERENCES setor(id_setor)
 );
 
--- Alerta 
-CREATE TABLE alerta (
-  id_alerta BIGINT AUTO_INCREMENT PRIMARY KEY,
-  fk_controlador INT NOT NULL,
-  `timestamp` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  cpu_pct DECIMAL(5,2),
-  ram_pct DECIMAL(5,2),
-  disk_pct DECIMAL(5,2),
-  tipo VARCHAR(80),
-  mensagem TEXT,
-  CONSTRAINT fk_alerta_controlador FOREIGN KEY (fk_controlador) REFERENCES controlador(id_controlador) ON DELETE CASCADE
-) ;
+-- Parametrização dos alertas
+CREATE TABLE parametrizacao_alerta(
+  fk_empresa INT NOT NULL,
+  fk_setor INT NOT NULL,
+  regras JSON,
+  componente VARCHAR(30),
+  valor_min DECIMAL(5,2),
+  valor_max DECIMAL(5,2),
+  CONSTRAINT fk_parametro_empresa FOREIGN KEY (fk_empresa) REFERENCES empresa(id_empresa),
+  CONSTRAINT fk_parametro_setor FOREIGN KEY (fk_setor) REFERENCES setor(id_setor),
+  PRIMARY KEY (fk_empresa, fk_setor)
+);
