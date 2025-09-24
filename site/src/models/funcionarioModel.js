f1b3adc6b4f9ad5e02120ac02e0b771251950a7a
Joao-error404 <joao.oliveiraneto@sptech.school>
2025-09-24 11:15:30 -0300

    modificacao funcionarioModel

--- site/src/models/funcionarioModel.js
+++ site/src/models/funcionarioModel.js
@@ -1,12 +1,12 @@
 var database = require("../database/config");
 
 function buscar() {
-    var sql = `SELECT * FROM funcionario`;
+    var sql = `SELECT * FROM funcionario;`
     return database.executar(sql)
 }
 
 function cadastrar(nome, email, senha, setor) {
-    var sql = `INSERT INTO funcionario (${nome}, ${email}, ${senha}, 1, ${setor}, 1, null, 1, null)`
+    var sql = `INSERT INTO funcionario VALUES("${nome}", "${email}", "${senha}", 1, ${setor}, 1, null, 1, null);`
     return database.executar(sql)
 }
 
