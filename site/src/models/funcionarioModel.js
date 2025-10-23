c845e96fa8592ed6cf55533aa8ce30bf7d24771c
Breno-0 <breno.costa@sptech.school>
2025-10-23 10:59:44 -0300

    Ajuste edição senha funcionario (agora com criptografia)

--- site/src/models/funcionarioModel.js
+++ site/src/models/funcionarioModel.js
@@ -15,10 +15,10 @@ function buscarCargos() {
     var sql = `SELECT * FROM cargo;`
     return database.executar(sql)
 }
-
 function cadastrar(nome, email, senha, setor, cargo, empresa) {
     var sql = `INSERT INTO funcionario(nome, email, senha_hash, fk_empresa, fk_setor, fk_cargo) VALUES("${nome}", "${email}", SHA2('${senha}', 256), ${empresa}, ${setor}, ${cargo});`
     return database.executar(sql)
+    
 }
 
 function buscarSetor(empresa) {
@@ -27,7 +27,7 @@ function buscarSetor(empresa) {
 }
 
 function editar(id, nome, email, senha, setor, cargo, empresa) {
-    var sql = `UPDATE funcionario SET nome="${nome}", email="${email}", senha_hash="${senha}", fk_setor=${setor}, fk_cargo=${cargo}, fk_empresa=${empresa} WHERE id_funcionario=${id};`
+    var sql = `UPDATE funcionario SET nome="${nome}", email="${email}", senha_hash="SHA2('${senha}', 256)", fk_setor=${setor}, fk_cargo=${cargo}, fk_empresa=${empresa} WHERE id_funcionario=${id};`
     return database.executar(sql)
 }
 
